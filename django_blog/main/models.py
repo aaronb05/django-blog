dd17f589eb2dbd95803a7aafd9df51a3dc00db37
aaronb05 <aboyles05@gmail.com>
2022-05-03 14:40:25 -0400

    added blog post and comment classes. initial migration successfull

--- django_blog/main/models.py
+++ django_blog/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=200)
+    Body = models.CharField(max_length=10000)
+    Published = models.BooleanField()
+    Archived = models.BooleanField()
+    Created = models.DateTimeField()
+    Updated = models.DateTimeField()
+
+    # create hash set of comments
+
+
+class Comment(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    Author = models.CharField(max_length=200)
+    Body = models.CharField(max_length=1000)
+    Created = models.DateTimeField()
+    Updated = models.DateTimeField()
+
+    # create virtual collection of blog posts
+    # create virtual collection of Authors
+
+
